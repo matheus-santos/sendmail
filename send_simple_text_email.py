b9ee08e0b59d41aa00b044b162345099fee16c52
Matheus Santos <matheus.cesario.santos@gmail.com>
2015-09-02 22:03:15 -0300

    1st commit

---
+++ send_simple_text_email.py
@@ -0,0 +1,30 @@
+# Import smtplib for the actual sending function
+import smtplib
+
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+
+# Variables
+template = "templates/hello_world.html"
+me = "matheus.cesario.santos@gmail.com"
+you = "mts.cesario@gmail.com"
+
+# Open a plain text file for reading.  For this example, assume that
+# the text file contains only ASCII characters.
+fp = open(template, "rb")
+
+# Create a text/plain message
+msg = MIMEText(fp.read())
+fp.close()
+
+# me == the sender's email address
+# you == the recipient's email address
+msg["Subject"] = "The contents of templates/hello_world.html"
+msg["From"] = me
+msg["To"] = you
+
+# Send the message via our own SMTP server, but don't include the
+# envelope header.
+s = smtplib.SMTP("localhost")
+s.sendmail(me, you, msg.as_string())
+s.quit()
